c7ffdcf352b025bb943494bb1a26fa5580623303
Camila Denise Goncalves <86990957+CamilaDen@users.noreply.github.com>
2022-10-26 21:17:07 -0300

    Initial commit

---
+++ dominio/Especialidad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Especialidad
+    {
+        public String Codigo { get; set; }
+        public String Nombre { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ tp-cuatrimestral-goncalves-gines/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tp_cuatrimestral_goncalves_gines.Global" Language="C#" %>
